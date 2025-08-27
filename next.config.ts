33f6dcc7808f9df730616117fbcb2f3f86a537a8
grantdoesmktg <grantdoesmktg@gmail.com>
2025-08-27 13:12:33 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Let builds succeed even if ESLint finds issues
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
