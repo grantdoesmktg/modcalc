6fb27a2c918756f757a602b953be998f4f3efb97
grantdoesmktg <grantdoesmktg@gmail.com>
2025-08-29 11:11:59 -0500

    Update VehiclePickerClient.tsx

--- src/components/VehiclePickerClient.tsx
+++ src/components/VehiclePickerClient.tsx
@@ -1,8 +1,15 @@
 'use client';
 import VehicleSelector from './VehicleSelector';
 
-export default function VehiclePickerClient() {
-  // You can add an onChange handler later if you want to lift the selection up.
-  return <VehicleSelector />;
-}
+type Props = {
+  onChange?: (selection: {
+    year?: number;
+    make?: string;
+    model?: string;
+    trim_label?: string;
+  }) => void;
+};
 
+export default function VehiclePickerClient({ onChange }: Props) {
+  return <VehicleSelector onChange={onChange} />;
+}
